340e894b3fdd10fa0ca0412e6120ee49d33fc1c0
Gurusundesh Khalsa <gurusundesh@khalsa.com>
2016-06-22 16:45:08 -0600

    add filter

--- filter.js
+++ filter.js
@@ -5,12 +5,3 @@ var worker = function(collection){
 };
 
 module.exports = worker;
-
-
-  //  var _ = require("lodash");
-   //
-  //  var worker = function(collection) {
-  //    return _.filter(collection, {'active': true })
-  //  };
-   //
-  //  module.exports = worker
