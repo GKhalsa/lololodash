a8c06a3111c06f09f047c46da1926405961bf7c1
Gurusundesh Khalsa <gurusundesh@khalsa.com>
2016-06-23 17:05:15 -0600

    add reduce

---
+++ reduce.js
@@ -0,0 +1,17 @@
+var _ = require("lodash");
+
+ var worker = function(collection) {
+
+  var grouped = _.groupBy(collection, 'article');
+
+  var mapped = _.map(grouped, function(array, key){
+    return { article: parseInt(key), total_orders: reduction(array) }
+  })
+
+  function reduction(array){
+    return _.reduce(array, function(sum, object){ return sum + object.quantity}, 0)
+  }
+
+  return mapped.reverse();
+ }
+ module.exports = worker;
